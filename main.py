a9d5b1b3d2982512e6c65173ac2e51d945b6b2e8
Ethan Phoenix <ethanjessephoenix@gmail.com>
2024-02-13 14:42:56 +0700

    leaving blank space for file link

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from translate import Translator
 
 translator = Translator(to_lang='zh')
 
-with open('/Users/ethanphoenix/Desktop/ztm-python/translator/translatorapp.txt', mode ='r') as my_file:
+with open('ADD FILE', mode ='r') as my_file:
     text = my_file.read()
     translation = translator.translate(text)
     print(translation)
-    with open('/Users/ethanphoenix/Desktop/ztm-python/translator/translatorapp2.txt', mode='w') as my_file2:
+    with open('ADD FILE', mode='w') as my_file2:
         my_file2.write(translation)
 
 
